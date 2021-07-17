0abc123ada9bd7b8cc0d6aecdb3b1ab1a2f12167
lupeipei_glo <lupeipei_glo>
2021-07-17 10:03:09 +0800

    Initial commit

---
+++ dynamic-rule_engine/src/main/java/com/lpp/beans/LogBean.java
@@ -0,0 +1,35 @@
+package com.lpp.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class LogBean {
+
+    private String account        ;
+    private String appId          ;
+    private String appVersion     ;
+    private String carrier        ;
+    private String deviceId       ;
+    private String deviceType     ;
+    private String ip             ;
+    private double latitude       ;
+    private double longitude      ;
+    private String netType        ;
+    private String osName         ;
+    private String osVersion      ;
+    private String releaseChannel ;
+    private String resolution     ;
+    private String sessionId      ;
+    private long timeStamp        ;
+    private String eventId        ;
+    private Map<String,String> properties;
+
+}
---
+++ dynamic-rule_engine/src/main/java/com/lpp/beans/ResultBean.java
@@ -0,0 +1,11 @@
+package com.lpp.beans;
+
+import lombok.Data;
+
+@Data
+public class ResultBean {
+
+    private String ruleId;
+    private String deviceId;
+    private long timeStamp;
+}
